4f05845b9b3d72219fa1b7c7571d0991a384ee9f
Mathias <tisen2013@gmail.com>
2025-03-28 13:47:07 +0100

    Use jinja

--- src/app.py
+++ src/app.py
@@ -1,17 +1,16 @@
 from flask import Flask, render_template
 
 app = Flask(__name__, template_folder='presentation/templates')
-app.jinja_env.add_extension('pypugjs.ext.jinja.PyPugJSExtension')
 
 
 @app.route('/')
 def index():
-    return render_template('index.pug')
+    return render_template('index.html')
 
 
 @app.route('/sign-up')
 def sign_up():
-    return render_template('sign-up.pug')
+    return render_template('sign-up.html')
 
 
 if __name__ == '__main__':
