441844d93ee27b038f4f4af82eac7ff46e8f1ceb
Mathias <tisen2013@gmail.com>
2025-04-10 15:29:40 +0200

    Fill page
    Updated:
    - Fills  search page with books on desktop

--- src/presentation/views.py
+++ src/presentation/views.py
@@ -11,7 +11,7 @@ def index(request):
 
 def search(request):
     page = request.GET.get("page", 1)
-    per_page = 50
+    per_page = 52
     sort = request.GET.get("sort", "title")
     order = request.GET.get("order", "asc")
     view = request.GET.get("view", "grid")
